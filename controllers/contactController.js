258b00c89e011f43230384fde11e3f86fe3af7d4
MhdRayanBinSN <rayan6203@gmail.com>
2025-01-02 19:20:52 +0530

    Registration authentication Completed

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -4,7 +4,7 @@ const Contact = require("../models/contactModel")
 //@desc get all contacts
 //@route GET /api/contacts
 //@acces public
-const getContacts = asyncHandler(async(req,res)=>{
+const getContacts = asyncHandler(async (req, res) => {
     const contacts = await Contact.find();
     res.status(200).json(contacts);
     //res.status(200).json({message :"Get all contacts from server"});
@@ -13,10 +13,10 @@ const getContacts = asyncHandler(async(req,res)=>{
 //@desc get  contact
 //@route GET /api/contacts/:id
 //@acces public
-const getContact = asyncHandler(async(req,res)=>{
+const getContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id)
     //if contact not found
-    if(!contact){
+    if (!contact) {
         res.status(404);
         throw new Error("Contact Not Found")
     }
@@ -27,26 +27,26 @@ const getContact = asyncHandler(async(req,res)=>{
 //@desc create  contact
 //@route post /api/contacts/
 //@acces public
-const createContact = asyncHandler(async(req,res)=>{
-    console.log("The request body :",req.body);
-    const {name,email,phone} = req.body
-    if(!name || !email || !phone){
+const createContact = asyncHandler(async (req, res) => {
+    console.log("The request body :", req.body);
+    const { name, email, phone } = req.body
+    if (!name || !email || !phone) {
         res.status(400);
         throw new ("All fields are mandatory")
     }
     const contact = await Contact.create({
-        name,email,phone,
+        name, email, phone,
     })
     res.status(201).json(contact)
-    res.status(201).json({message: `create contact`});
+    res.status(201).json({ message: `create contact` });
 })
 
 //@desc update  contact
 //@route PUT /api/contacts/:id
 //@acces public
-const updateContact = asyncHandler(async(req,res)=>{
+const updateContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id)
-    if(!contact){
+    if (!contact) {
         res.status(404)
         throw new Error("contact not Found")
     }
@@ -54,7 +54,7 @@ const updateContact = asyncHandler(async(req,res)=>{
     const updatedContact = await Contact.findByIdAndUpdate(
         req.params.id, //get contact
         req.body,     //new by that updated
-        {new:true}    //query option 
+        { new: true }    //query option 
     )
     res.status(200).json(updatedContact)
     //res.status(200).json({message: `update contact ${req.params.id}` });
@@ -63,21 +63,21 @@ const updateContact = asyncHandler(async(req,res)=>{
 //@desc delete  contact
 //@route Delete /api/contacts/:id
 //@acces public
-const deleteContact = asyncHandler(async(req,res)=>{
+const deleteContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id)
-    if(!contact){
+    if (!contact) {
         res.status(404)
         throw new error("Contact NOT Found")
     }
-    try{
+    try {
         await Contact.deleteOne();
         res.status(200).json(contact)
-    }catch(err){
+    } catch (err) {
         console.log(err);
-        
+
     }
     //res.status(200).json({message: `delete contact ${req.params.id}` });
 })
 
 
-module.exports ={ getContacts,getContact,createContact,updateContact,deleteContact}
+module.exports = { getContacts, getContact, createContact, updateContact, deleteContact }
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,25 +1,55 @@
 const asyncHandler = require("express-async-handler")
-
+const User = require("../models/userModel")
+const bcrypt = require("bcrypt")
 //@desc registration
 //@route POST /api/users/register
 //@acces public
 const registerUser = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+    //if any user data field is empty
+    if (!username || !email || !password) {
+        res.status(400);
+        throw new Error({message:"All fields are mandatory"})
+    }
+    //check the user is found or not
+    const userAvailable = await User.findOne({email}) // check email found
+    if(userAvailable){
+        res.status(400)
+        throw new Error("User Already Existed")
+    }
+    //password hashing
+    const hashedPassword = await bcrypt.hash(password,10)  //(raw pass,salt round{no of chars})
+    console.log("Hashed pass : ",hashedPassword);
+
+    //user registration
+    const user = await User.create({
+        username,
+        email,
+        password:hashedPassword,
+    })
+    console.log(`user created ${user}`);
+    if(user){
+        res.status(201).json({_id:user.id,email:user.email})
+    }else{
+        res.status(400);
+        throw new Error("user data is not valid")
+    }
     res.json({ message: "Register the user" })
 })
 
 //@desc login
 //@route POST /api/users/login
 //@acces public
-const loginUser = asyncHandler(async(req,res)=>{
-    res.json({message:"Login user"})
+const loginUser = asyncHandler(async (req, res) => {
+    res.json({ message: "Login user" })
 })
 
 //@desc current
 //@route POST /api/users/current
 //@acces private
-const currentUser = asyncHandler(async(req,res)=>{
-    res.json({message:"current user"})
+const currentUser = asyncHandler(async (req, res) => {
+    res.json({ message: "current user" })
 })
 
 
-module.exports = { registerUser ,loginUser,currentUser}
+module.exports = { registerUser, loginUser, currentUser }
