16e28831c391d995f5d7cb1c30f1a8276c11c953
MhdRayanBinSN <rayan6203@gmail.com>
2025-01-06 21:27:25 +0530

    Compnents structured

---
+++ front-end/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header"
+import AddContact from "./AddContact"
+import ContactCard from "./ContactCard"
+import ContactList from "./ContactList"
+
+function App() {
+  return (
+    <div>Hello World</div>
+  );
+}
+
+export default App;
