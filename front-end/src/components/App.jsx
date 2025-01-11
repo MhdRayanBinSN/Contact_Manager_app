ea857aa996a04e85ffb5cfcbdf06a9b71e455d29
MhdRayanBinSN <rayan6203@gmail.com>
2025-01-11 14:53:32 +0530

    useeffect for retrieval contact

--- front-end/src/components/App.jsx
+++ front-end/src/components/App.jsx
@@ -1,23 +1,34 @@
-import React ,{useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import Header from "./Header"
-import AddContact from "./AddContact"
-import ContactList from "./ContactList"
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
 
 function App() {
-  const [contacts ,setContacts]=useState([])
-  const addContactHandler = (contact)=>{
-    console.log(contact);
-    setContacts([...contacts,contact])
-    
-  }
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    setContacts([...contacts, contact]);
+  };
+//storing contact even after refresh
+  useEffect(() => {
+    const retrievedContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retrievedContacts) setContacts(retrievedContacts);
+  }, []);
+
+  useEffect(() => {
+    if (contacts.length > 0) {
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+    }
+  }, [contacts]);
+
   return (
-    <div className='ui container center'>
+    <div className="ui container center">
       <Header />
-      <AddContact addContactHandler={addContactHandler}/>
-      <ContactList contacts={contacts}/>
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} />
     </div>
-    
   );
 }
 
